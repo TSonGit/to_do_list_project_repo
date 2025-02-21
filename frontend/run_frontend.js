92978a6fcc328f514333c37c93d14377044665f9
thomas_TableSurfer <tshort00011@gmail.com>
2025-02-21 16:43:12 -0600

    Added public folder; Added route public/stylesheets/style.css to project; Added external css styling to the list_display.ejs file; Added notes.txt to help keep up with project design ideas.

--- frontend/run_frontend.js
+++ frontend/run_frontend.js
@@ -5,6 +5,9 @@ const app = express();
 //Sets up the view for port 3000.
 app.set('view engine', 'ejs');
 
+//Serves static files from the public folder.
+app.use(express.static('public'));
+
 //Test if file can communicate with server: localhost:3000
 app.get('/', (req, res) => {
     console.log('Ran for a test.');
