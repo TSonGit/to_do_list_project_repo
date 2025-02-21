7713f66173c87f360497cc93ea2fb3c3847bba9f
thomas_TableSurfer <tshort00011@gmail.com>
2025-02-21 15:32:05 -0600

    First commit.

---
+++ frontend/run_frontend.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const { name } = require('ejs');
+const app = express();
+
+//Sets up the view for port 3000.
+app.set('view engine', 'ejs');
+
+//Test if file can communicate with server: localhost:3000
+app.get('/', (req, res) => {
+    console.log('Ran for a test.');
+    res.send('Ran from run_frontend.js');
+})
+
+// Start the server on port 3000
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+  });
