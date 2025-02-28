14de62ee43dd3155c0b072ad3a43260e6778e366
thomas_TableSurfer <tshort00011@gmail.com>
2025-02-27 21:37:09 -0600

    Added path to db. Created sqlQuery; Confirmed to work. Attempted to update addTask: Added due_date, task_status; Still needs work on submitting form from this function to app.post in run_frontend.

--- frontend/run_frontend.js
+++ frontend/run_frontend.js
@@ -1,6 +1,7 @@
+const sqlite3 = require('sqlite3').verbose(); //new
+const path = require('path'); //new
 const express = require('express');
 const { name } = require('ejs');
-const path = require('path');
 const app = express();
 
 //Sets up the view for port 3000.
@@ -8,20 +9,61 @@ app.set('view engine', 'ejs');
 
 //Serves static files from the public folder.
 app.use(express.static('public'));
+//For .post requests.
+app.use(express.urlencoded({ extended: true }));
+//Path to Database.
+const dbPath = './DBeaver/to-do-persistence-DB';
 
-//Test if file can communicate with server: localhost:3000
-app.get('/', (req, res) => {
-    console.log('Ran for a test.');
-    res.send('Ran from run_frontend.js');
+// Opens the database
+const db = new sqlite3.Database(dbPath, (err) => {
+    if (err) {
+        console.error('Error opening database:', err.message);
+    } else {
+        console.log('Connected to the SQLite database.');
+    }
 });
 
+// Opens the app in localhost3000;
 app.get('/to-do_list', (req, res) => {
-    console.log('Ran from .get for to-do_list.');
-    res.render('list_display');
-    console.log('Past the render here!')
+    const sqlQuery =
+    `SELECT *
+     FROM TaskInfo
+    `;
+
+    db.all(sqlQuery, [], (err, sqlResult) => {
+        if (err) {
+            console.log("Error attempting sqlQuery.")
+            return res.status(500).send('Error retrieving data from database');
+        }
+
+        if (sqlResult) {
+            // console.log(sqlResult);
+            res.render('list_display');
+        }
+    });
 });
+
+//Adds task to db.
+app.post('/add_task', (req, res) => {
+    const {task_name, task_desc, task_stat, due_date} = req.body;
+    //Inserts information into db.
+    const addTaskQuery = 
+    `INSERT INTO TaskInfo ("Task_Name", "Task_Description", "Task_Status", "Due_Date")
+     VALUES ("${task_name}", "${task_desc}", "${task_stat}", "${due_date}")
+    `;
+
+    db.run(addTaskQuery, [], function (err) {
+        if (err) {
+            console.error('Error adding task:', err.message);
+            return res.status(500).send('Error adding task.');
+        }
+    });
+    // res.redirect('/to-do_list');
+    console.log('ran the post.')
+});
+
 
 // Start the server on port 3000
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
-  });
+});
--- public/javascript/helper.js
+++ public/javascript/helper.js
@@ -1,39 +1,58 @@
+// const { constants } = require("buffer");
+
 function addTask() {
   const task_name_result = promptUser_TaskName();
-  // Creates Elements.
+  if (task_name_result) {
+    console.log('in name if');
+    const due_date_result = promptUser_DueDate();
+
+    // Creates Elements.
     const newListElement = document.createElement("li");
     const newDivElement = document.createElement("div");
     const newParagraphElement = document.createElement("p");
 
-  //Adds ID's to Elements.
-    // newListElement.classList.add("task_name");
-    // newDivElement.classList.add("description_div");
-    // newParagraphElement.classList.add("task_description");
+    //If the user enters a valid task name into the prompt,
+    //append those results to the proper elements, and 
+    //have them show in the task_board.
+    if (due_date_result) {
+      console.log('in due if');
+      const task_description_result = promptUser_TaskDescription();
+      const task_status = 'Incomplete.';
+      //Appends to Elements.
+      newListElement.append(task_name_result);
+      newListElement.append(newDivElement);
+      newParagraphElement.append(task_description_result);
+      newDivElement.append("> Task Description: ");
+      newDivElement.append(newParagraphElement);
 
-  if (task_name_result) {
-    const task_description_result = promptUser_TaskDescription();
-    //Appends to Elements.
-    newListElement.append(task_name_result);
-    newListElement.append(newDivElement);
-    newParagraphElement.append(task_description_result);
-    newDivElement.append("> Task Description: ");
-    newDivElement.append(newParagraphElement);
+      //Appends to Document.
+      document.getElementById("task_list").append(newListElement);
 
-    //Appends to Document.
-    document.getElementById("task_list").append(newListElement);
-    // document.getElementsByClassName("task_name").append(newDivElement);
+      //Sets the classlist of newDivElement to hidden as default.
+      newDivElement.classList.add('hidden');
 
-    newDivElement.classList.add('hidden');
-    //Adds attributes to Elements.
-    newListElement.addEventListener("click", checkViewStatus);
-      
+      //Adds attributes to Elements.
+      //Upon being clicked, a div will switch between being
+      //hidden or shown.
+      newListElement.addEventListener("click", checkViewStatus);
+
+      const form = createForm(task_name_result, task_description_result, task_status, due_date_result);
+      console.log(form);
+      submitNewForm(form);
+
+    } else {
+      //If the user enters an invalid due date, or cancels the request.
+      window.alert("User did not include a valid due date or cancelled the request.");
+    }
   } else {
-    window.alert("User cancelled prompt.");
+    //If the user enters an invalid task name, or cancels the request.
+    window.alert("User did not include a valid task name or cancelled the request.");
   }
 };
 
+//Prompts user for a task name, then returns it in a string.
 function promptUser_TaskName() {
-    let text;
+  let text;
   let person = prompt("Please title your task:", "Task Name");
   if (person != null && person != "") {
     text = person;
@@ -41,8 +60,9 @@ function promptUser_TaskName() {
   }
 }
 
+//Prompts the user for a task description, then returns it in a string.
 function promptUser_TaskDescription() {
-    let text;
+  let text;
   let person = prompt("Please describe your task:", "Task Description");
   if (person == null || person == "") {
     text = "User did not include a description."
@@ -52,6 +72,58 @@ function promptUser_TaskDescription() {
   return text;
 }
 
+function promptUser_DueDate() {
+  let text;
+  let person = prompt("Please enter a Due Datea:", "dd/mm/yy");
+  if (person != null && person != "") {
+    text = person;
+    return text;
+  }
+};
+
+function createForm(name, desc, stat, date) {
+  console.log('ran from beginning of createForm.')
+  //Create Form
+  const newForm = document.createElement('form');
+  newForm.classList.add('new_task')
+  newForm.action = '/to-do_list/add_task';
+  newForm.method = 'POST';
+
+  //Create input: task_name
+  const inputName = document.createElement('input1');
+  inputName.type = 'text';
+  inputName.name = 'task_name';
+  inputName.append(name);
+  newForm.appendChild(inputName);
+
+  //Create input: task_desc
+  const inputDesc = document.createElement('input2');
+  inputDesc.type = 'text';
+  inputDesc.name = 'task_desc';
+  inputDesc.append(desc);
+  newForm.appendChild(inputDesc);
+
+  //Create input: task_stat
+  const inputStat = document.createElement('input3');
+  inputStat.type = 'text';
+  inputStat.name = 'task_stat';
+  inputStat.append(stat);
+  newForm.appendChild(inputStat);
+
+  //Create input: due_date
+  const inputDate = document.createElement('input4');
+  inputDate.type = 'text';
+  inputDate.name = 'due_date';
+  inputDate.append(date);
+  newForm.appendChild(inputDate);
+
+  document.body.appendChild(newForm);
+
+  console.log('ran from bottom of createForm.')
+}
+
+//Finds the current classlist for the description_div: hidden or shown,
+//then calls the appropriate function to swap them.
 function checkViewStatus(event) {
   const theDiv = event.target.querySelector("div");
   console.log(event.target.querySelector("div"));
@@ -62,12 +134,22 @@ function checkViewStatus(event) {
   };
 }
 
+//removes the hidden classlist, and replaces it with shown.
 function removeHidden_AddShown(element) {
   element.classList.remove("hidden");
   element.classList.add("shown");
 }
 
+//removes the shown classlist, and replaces it with hidden.
 function removeShown_AddHidden(element) {
   element.classList.remove("shown");
   element.classList.add("hidden");
 }
+
+function submitNewForm(event) {
+  const theForm = event.target.querySelector("form");
+  console.log(event.target.querySelector("form"));
+  if (theForm.classList.contains('new_task')) {
+    theForm.submit();
+  }
+} 
